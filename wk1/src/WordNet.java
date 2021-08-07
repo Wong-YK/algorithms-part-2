35b3074e60e62ed4060d4fd4bcb775f763ad6d66
Wong-YK <59433231+Wong-YK@users.noreply.github.com>
2021-08-07 17:41:16 +0100

    first implementation of sap()

--- wk1/src/WordNet.java
+++ wk1/src/WordNet.java
@@ -1,9 +1,12 @@
 import edu.princeton.cs.algs4.*;
+import java.util.ArrayList;
 
 public class WordNet {
 
-    private BST<String, Integer> nouns = new BST<String, Integer>();
+    private BST<String, Integer> nounsBST = new BST<String, Integer>();
+    private ArrayList<String> synsetsList = new ArrayList<String>();
     private Digraph dag;
+    private int root;
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
@@ -15,9 +18,11 @@ public class WordNet {
         while (in.hasNextLine()) {
             String line = in.readLine();
             String[] fields = line.split(",");
+            String synset = fields[1];
+            this.synsetsList.add(synset);
             String[] synonyms = fields[1].split("\s");
             for (String noun: synonyms) {
-                this.nouns.put(noun, id);
+                this.nounsBST.put(noun, id);
             }
             id++;
         }
@@ -33,19 +38,24 @@ public class WordNet {
                 }
             }
         }
-        if (! (new Topological(this.dag).hasOrder()) ) {
+        Topological t = new Topological(this.dag);
+        if (!t.hasOrder()) {
                 throw new IllegalArgumentException();
+        }
+        for (int vertex: t.order()) {
+            this.root = vertex;
+            break;
         }
     }
 
     // returns all WordNet nouns
     public Iterable<String> nouns() {
-        return this.nouns.keys();
+        return this.nounsBST.keys();
     }
 
     // is the word a WordNet noun?
     public boolean isNoun(String word) {
-        return this.nouns.contains(word);
+        return this.nounsBST.contains(word);
     }
 
     // distance between nounA and nounB (defined below)
@@ -56,7 +66,24 @@ public class WordNet {
     // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
     // in a shortest ancestral path (defined below)
     public String sap(String nounA, String nounB) {
-        return null;
+        int idA = this.nounsBST.get(nounA);
+        int idB = this.nounsBST.get(nounB);
+        BreadthFirstDirectedPaths pathsA = new BreadthFirstDirectedPaths(this.dag, idA);
+        BreadthFirstDirectedPaths pathsB = new BreadthFirstDirectedPaths(this.dag, idB);
+        Digraph reverse = this.dag.reverse();
+        Queue<Integer> q = new Queue<>();
+        q.enqueue(this.root);
+        int currentVertex = -1;
+        while (!q.isEmpty()) {
+            currentVertex = q.dequeue();
+            Iterable<Integer> adjacentVertices = reverse.adj(currentVertex);
+            for (int vertex: adjacentVertices) {
+                if (pathsA.hasPathTo(vertex) && pathsB.hasPathTo(vertex)) {
+                    q.enqueue(vertex);
+                }
+            }
+        }
+        return this.synsetsList.get(currentVertex);
     }
 
     // do unit testing of this class
