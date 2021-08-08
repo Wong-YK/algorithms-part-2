306cf565c5072c606d8ceb6c151cd9801a42563e
Wong-YK <59433231+Wong-YK@users.noreply.github.com>
2021-08-08 16:51:20 +0100

    new unit test for distance()

--- wk1/src/SAPTest.java
+++ wk1/src/SAPTest.java
@@ -11,10 +11,28 @@ public class SAPTest {
         g.addEdge(1, 0);
         g.addEdge(2, 0);
         g.addEdge(3, 0);
+        g.addEdge(3, 1);
         g.addEdge(4, 1);
         g.addEdge(5, 4);
         g.addEdge(6, 4);
         SAP sap = new SAP(g);
         assertEquals(0, sap.ancestor(1, 2));
     }
+
+    @Test
+    // common ancestor is 0 and distance is 2
+    public void ancestorTest2() {
+        Digraph g = new Digraph(7);
+        g.addEdge(1, 0);
+        g.addEdge(2, 0);
+        g.addEdge(3, 0);
+        g.addEdge(3, 1);
+        g.addEdge(4, 1);
+        g.addEdge(5, 4);
+        g.addEdge(6, 4);
+        SAP sap = new SAP(g);
+        assertEquals(1, sap.ancestor(3, 6));
+    }
+
+
 }
