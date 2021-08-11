0f1a20333aa5477d0b14e06de03e2b472fa0755b
Wong-YK <59433231+Wong-YK@users.noreply.github.com>
2021-08-11 11:18:35 +0100

    implemented outcast()

--- wk1/src/Outcast.java
+++ wk1/src/Outcast.java
@@ -8,7 +8,21 @@ public class Outcast {
     }
 
     // given an array of WordNet nouns, return an outcast
-    public String outcast(String[] nouns) { return null; }
+    public String outcast(String[] nouns) {
+        int maxDistance = 0;
+        String result = nouns[0];
+        for (int i = 0; i < nouns.length; i++) {
+            int distanceSum = 0;
+            for (int j = 0; j < nouns.length; j++) {
+                distanceSum += this.wordnet.distance(nouns[i], nouns[j]);
+                }
+            if (distanceSum > maxDistance) {
+                maxDistance = distanceSum;
+                result = nouns[i];
+            }
+        }
+        return result;
+    }
 
     // see test client below
     public static void main(String[] args) {}
